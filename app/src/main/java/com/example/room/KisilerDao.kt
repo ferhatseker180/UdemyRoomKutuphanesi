98130402f0abe5a2667a9c2ca0a50d48b3979324
ferhatseker180 <ferhatseker180@gmail.com>
2023-03-16 18:41:38 +0300

    Room metodundaki tüm fonsiyonlar uygulandı, işlem tamamlandı.

--- app/src/main/java/com/example/room/KisilerDao.kt
+++ app/src/main/java/com/example/room/KisilerDao.kt
@@ -1,8 +1,6 @@
 package com.example.room
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface KisilerDao {
@@ -11,5 +9,25 @@ interface KisilerDao {
 
     @Insert
     suspend fun kisiEkle(kisi:Kisiler)
+
+    @Update
+    suspend fun kisiGuncelle(kisi: Kisiler)
+
+    @Delete
+    suspend fun kisiSil(kisi: Kisiler)
+
+    @Query("SELECT * FROM kisiler ORDER BY RANDOM() LIMIT 1") // Kayıtlar random şekilde sınırladığımız sayıda gelecek.
+    suspend fun rastgeleKisiGetir() : List<Kisiler>
+
+    @Query("SELECT * FROM kisiler WHERE kisi_ad like '%' || :aramaKelimesi || '%'")
+    suspend fun kisiAra(aramaKelimesi : String?) : List<Kisiler>
+
+    // Tek satır halinde kayıt getirme
+    @Query("SELECT * FROM kisiler WHERE kisi_id=:kisi_id")
+    suspend fun kisiGetir(kisi_id : Int) : Kisiler
+
+    // Kayıt Kontrol
+    @Query("SELECT count(*) FROM kisiler WHERE kisi_ad=:kisi_ad")
+    suspend fun kisiKontrol(kisi_ad : String) : Int
 
 }
--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -19,8 +19,15 @@ class MainActivity : AppCompatActivity() {
         vt = Veritabani.veritabaniErisim(this)!!
         kdao = vt.getkisilerDao()
 
-        kisileriYukle()
-        ekle()
+     //   kisileriYukle()
+      //   ekle()
+     //   guncelle()
+     //   sil()
+       // rastgele()
+      //  ara()
+      //  getir()
+        kontrol()
+
     }
     fun kisileriYukle() {
         val job = CoroutineScope(Dispatchers.Main).launch {
@@ -37,8 +44,74 @@ class MainActivity : AppCompatActivity() {
 
     fun ekle() {
         val job = CoroutineScope(Dispatchers.Main).launch {
-            val yeniKisi = Kisiler(0,"Ahmet",30)
+            val yeniKisi = Kisiler(0,"Ferhat",24)
+            val yeniKisi2 = Kisiler(0,"Ferhat",24)
+            val yeniKisi3 = Kisiler(0,"Figen",49)
             kdao.kisiEkle(yeniKisi)
+            kdao.kisiEkle(yeniKisi2)
+            kdao.kisiEkle(yeniKisi3)
+
+
+        }
+    }
+
+    fun guncelle() {
+
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val guncellenenKisi = Kisiler(3,"Yeni Ahmet",92)
+            kdao.kisiGuncelle(guncellenenKisi)
+        }
+    }
+
+    fun sil() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val silinenKisi = Kisiler(2,"",0)
+            kdao.kisiSil(silinenKisi)
+        }
+    }
+
+    fun rastgele() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val gelenListe = kdao.rastgeleKisiGetir()
+
+            for (k in gelenListe) {
+                Log.e("Kişi id",k.kisi_id.toString())
+                Log.e("Kişi ad",k.kisi_ad)
+                Log.e("Kişi yaş",k.kisi_yas.toString())
+
+            }
+        }
+    }
+
+    fun ara() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val gelenListe = kdao.kisiAra("h")
+
+            for (k in gelenListe) {
+                Log.e("Kişi id",k.kisi_id.toString())
+                Log.e("Kişi ad",k.kisi_ad)
+                Log.e("Kişi yaş",k.kisi_yas.toString())
+
+            }
+        }
+    }
+
+    fun getir() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val gelenKisi = kdao.kisiGetir(3)
+
+           Log.e("Kişi id",gelenKisi.kisi_id.toString())
+            Log.e("Kişi ad",gelenKisi.kisi_ad)
+            Log.e("Kişi yaş",gelenKisi.kisi_yas.toString())
+
+        }
+    }
+
+    fun kontrol() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val gelenSonuc = kdao.kisiKontrol("Ferhat")
+
+            Log.e("Kişi Kontrol",gelenSonuc.toString())
 
 
         }
