e77b77b93108d71decf4ebf824c70450c4898a5c
ferhatseker180 <ferhatseker180@gmail.com>
2023-03-16 18:01:23 +0300

    Kişi ekleme ve görüntüleme metodları eklendi.

--- app/src/main/java/com/example/room/KisilerDao.kt
+++ app/src/main/java/com/example/room/KisilerDao.kt
@@ -1,10 +1,15 @@
 package com.example.room
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface KisilerDao {
 @Query("SELECT * FROM kisiler")
     suspend fun tumKisiler() : List<Kisiler>
+
+    @Insert
+    suspend fun kisiEkle(kisi:Kisiler)
+
 }
--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var vt : Veritabani
@@ -14,5 +19,29 @@ class MainActivity : AppCompatActivity() {
         vt = Veritabani.veritabaniErisim(this)!!
         kdao = vt.getkisilerDao()
 
+        kisileriYukle()
+        ekle()
     }
+    fun kisileriYukle() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val gelenListe = kdao.tumKisiler()
+
+            for (k in gelenListe) {
+                Log.e("Kişi id",k.kisi_id.toString())
+                Log.e("Kişi ad",k.kisi_ad)
+                Log.e("Kişi yaş",k.kisi_yas.toString())
+
+            }
+        }
+    }
+
+    fun ekle() {
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            val yeniKisi = Kisiler(0,"Ahmet",30)
+            kdao.kisiEkle(yeniKisi)
+
+
+        }
+    }
+
 }
